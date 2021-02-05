8c008df1e46ea2fe3495635528439081f4ed1a14
marcorossignoli20 <marcorox48@gmail.com>
2021-02-05 17:10:39 +0100

    ✅ first part of day 4 done

---
+++ 4/day4.py
@@ -0,0 +1,15 @@
+input = open("input.txt", "r").read().split("\n\n")
+valid_count = 0
+
+for passport in input:
+    tokens = passport.split()
+    if len(tokens) == 8:
+        valid_count = valid_count + 1
+    elif len(tokens) == 7:
+        for tok in tokens:
+            parameter = tok.split(":")
+            if parameter[0] == "cid":
+                valid_count = valid_count - 1
+                break
+        valid_count = valid_count + 1
+print(valid_count)
